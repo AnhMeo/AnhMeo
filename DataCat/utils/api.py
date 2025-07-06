796720a7c3bfd318e71a4221ef093b2e31bbf367
AnhMeo <u799696294@byui.edu>
2025-07-06 10:20:32 +0700

    updated API connection

--- DataCat/utils/api.py
+++ DataCat/utils/api.py
@@ -8,12 +8,9 @@ API_KEY = os.getenv("VALORANT_API_KEY")
 HEADERS = {"TRN-Api-Key": API_KEY}
 
 def get_match_history(name: str, tag: str):
-    """
-    Fetch Valorant profile and match data using Tracker Network API.
-    Riot ID format: name='AnhMeo', tag='iuMai'
-    """
+    
     encoded_tag = tag.replace("#", "%23")
-    url = f"https://public-api.tracker.gg/v2/valorant/standard/profile/riot/{name}%23{tag}"
+    url = f"https://public-api.tracker.gg/v2/valorant/standard/profile/riot/AnhMeo%23iuMai"
 
     response = requests.get(url, headers=HEADERS)
 
