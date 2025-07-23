af9a62f199d98f2db9edbeae8f36c70f2cc21b3f
AnhMeo <u799696294@byui.edu>
2025-07-23 21:01:17 +0700

    added edit and delete features, along with completing the signin / signup page

--- CashCat/cashcat_project/cashcat/admin.py
+++ CashCat/cashcat_project/cashcat/admin.py
@@ -5,6 +5,6 @@ from .models import Transaction
 
 @admin.register(Transaction)
 class TransactionAdmin(admin.ModelAdmin):
-    list_display = ('transaction_type', 'category', 'amount', 'description', 'date')
-    list_filter = ('transaction_type', 'category')
-    search_fields = ('category', 'description')
+    list_display = ('user', 'transaction_type', 'category', 'amount', 'description', 'date')
+    list_filter = ('transaction_type', 'category', 'user')
+    search_fields = ('category', 'description', 'user__username')
--- CashCat/cashcat_project/cashcat/models.py
+++ CashCat/cashcat_project/cashcat/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import User
+
 class Transaction(models.Model):
     TRANSACTION_TYPES = [
         ('income', 'Income'),
         ('expense', 'Expense'),
     ]
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     category = models.CharField(max_length=50)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField(blank=True)
